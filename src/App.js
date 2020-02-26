84d11e656271972a6abde5b7dde8136f8a6c9e63
wesselt <wesselt@aule.net>
2020-02-26 17:26:58 +0100

    Login in separate component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+
+class Login extends React.Component {
+  render() {
+    const ynab_client_id =
+              '5f9fa9ef4fb1517961ea9d50498f73a4159a7d0918329ff2aae71bca0b553e06';
+    const redirect_url = 'https://bunq2ynab.aule.net/dev';
+    const url = `https://app.youneedabudget.com/oauth/authorize?` +
+      `client_id=${ynab_client_id}&` +
+      `redirect_uri=${redirect_url}&` +
+      `response_type=code`;
+    return (
+      <header className="App-header">
+          <Button variant="primary" href={url}>
+              Login with YNAB</Button>
+      </header>
+    )
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Login />
     </div>
   );
 }
